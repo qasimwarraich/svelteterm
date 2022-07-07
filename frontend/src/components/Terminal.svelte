4f615c5d00efab3b15468281d91a9515811c79c9
qasimwarraich <qasim.warraich@gmail.com>
2022-07-07 21:24:31 +0200

    feat(frontend): Implements TTY resizing API call

--- frontend/src/components/Terminal.svelte
+++ frontend/src/components/Terminal.svelte
@@ -1,26 +1,30 @@
 <script lang="ts">
     import '../assets/xterm.css';
     import './TerminalSpawnButton.svelte';
+    import { ResizeContainerPTY } from './api';
     import { AttachAddon } from 'xterm-addon-attach';
     import { onMount } from 'svelte';
-    import TerminalSpawnButton from './TerminalSpawnButton.svelte';
+
     export let containerid;
 
     onMount(async () => {
         const { Terminal } = await import('xterm');
         const term = new Terminal({
-            fontSize: 10,
-            rows: 68,
-            cols: 270,
+            fontSize: 12,
+            rows: 100,
+            cols: 180,
         });
         const socket = new WebSocket(
-            'wss://127.0.0.1:2376/containers/spamcontainer/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1'
+            `ws://0.0.0.0:6969/containers/${containerid}/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1`
         );
 
         const attachAddon = new AttachAddon(socket);
         term.loadAddon(attachAddon);
         term.open(document.getElementById('terminal-parent'));
+        let resize = { id: containerid, height: term.rows, width: term.cols };
+        ResizeContainerPTY(resize);
 
+        //Sets focus to terminal text input
         const textarea = document.getElementsByClassName('xterm-helper-textarea').item(0);
         (textarea as HTMLElement).focus();
     });
