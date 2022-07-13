9ce1d851d17842793848ec3c9ebae538274cf5a0
qasimwarraich <qasim.warraich@gmail.com>
2022-07-13 14:47:14 +0200

    feat(frontend): Adds info modal popup

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,19 +1,38 @@
 <script lang="ts">
     import { fade } from 'svelte/transition';
+    import InfoModal from './components/InfoModal.svelte';
     import Term from './components/Terminal.svelte';
     import TerminalSpawnButton from './components/TerminalSpawnButton.svelte';
     import type { Container } from './types/types';
     import './assets/fonts.css';
     let activesession: boolean;
     let container: Container;
+    let modal: InfoModal;
 </script>
 
 <main>
     <h1>Chistole: CLI Tutor</h1>
     <body>
-        <TerminalSpawnButton bind:activesession bind:container />
+        <InfoModal bind:this={modal}>
+            <h2>Welcome to the CLI Tutor Web Interface</h2>
+            <p>
+                This is web interface allows you to use the cli tutor program in a sandboxed
+                environment so you cannot hurt your own system. In order to initiate a new session
+                press the "Spawn Instance Button". This will spawn a web terminal and connect it to
+                a docker container where you will have access to the tool. You can the start the
+                program by typing <code> ./cli-tutor</code> in the terminal.
+                <br /><br />
+
+                Have fun!
+            </p>
+            <button class="infobutton" on:click={() => modal.hide()}>Close</button>
+        </InfoModal>
+
+        <div class="nav">
+            <TerminalSpawnButton bind:activesession bind:container />
+            <button class="infobutton" on:click={() => modal.show()}>Info</button>
+        </div>
         {#if activesession}
-            <Term containerid={container.ID} />
             <div transition:fade>
                 <Term containerid={container.ID} />
             </div>
@@ -45,6 +64,11 @@
         max-width: 14rem;
     }
 
+    code {
+        background-color: #5c7287;
+        color: white;
+    }
+
     footer {
         width: 100vw;
         bottom: 0;
@@ -58,5 +82,23 @@
         h1 {
             max-width: none;
         }
+    }
+
+    button:hover {
+        color: #fff;
+    }
+
+    .nav {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .infobutton {
+        background-color: #ffb852;
+        font: inherit;
+        margin: 1em;
+        padding: 0.5em;
+        border: none;
     }
 </style>
